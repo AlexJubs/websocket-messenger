37489a8ff4c37172db8654f537f65be6839f8327
alexjubs <redpod22@gmail.com>
2024-05-21 15:50:20 -0400

    nit

--- server/WebsocketServer.cpp
+++ server/WebsocketServer.cpp
@@ -4,7 +4,7 @@
 #include <functional>
 #include <iostream>
 
-//The name of the special JSON field that holds the message type for messages
+// The name of the special JSON field that holds the message type for messages
 #define MESSAGE_FIELD "__MESSAGE__"
 
 Json::Value WebsocketServer::parseJson(const string& json)
@@ -17,7 +17,7 @@ Json::Value WebsocketServer::parseJson(const string& json)
 
 string WebsocketServer::stringifyJson(const Json::Value& val)
 {
-	//When we transmit JSON data, we omit all whitespace
+	// When we transmit JSON data, we omit all whitespace
 	Json::StreamWriterBuilder wbuilder;
 	wbuilder["commentStyle"] = "None";
 	wbuilder["indentation"] = "";
@@ -27,28 +27,28 @@ string WebsocketServer::stringifyJson(const Json::Value& val)
 
 WebsocketServer::WebsocketServer()
 {
-	//Wire up our event handlers
+	// Wire up our event handlers
 	this->endpoint.set_open_handler(std::bind(&WebsocketServer::onOpen, this, std::placeholders::_1));
 	this->endpoint.set_close_handler(std::bind(&WebsocketServer::onClose, this, std::placeholders::_1));
 	this->endpoint.set_message_handler(std::bind(&WebsocketServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
 	
-	//Initialise the Asio library, using our own event loop object
+	// Initialise the Asio library, using our own event loop object
 	this->endpoint.init_asio(&(this->eventLoop));
 }
 
 void WebsocketServer::run(int port)
 {
-	//Listen on the specified port number and start accepting connections
+	// Listen on the specified port number and start accepting connections
 	this->endpoint.listen(port);
 	this->endpoint.start_accept();
 	
-	//Start the Asio event loop
+	// Start the Asio event loop
 	this->endpoint.run();
 }
 
 size_t WebsocketServer::numConnections()
 {
-	//Prevent concurrent access to the list of open connections from multiple threads
+	// Prevent concurrent access to the list of open connections from multiple threads
 	std::lock_guard<std::mutex> lock(this->connectionListMutex);
 	
 	return this->openConnections.size();
@@ -56,17 +56,17 @@ size_t WebsocketServer::numConnections()
 
 void WebsocketServer::sendMessage(ClientConnection conn, const string& messageType, const Json::Value& arguments)
 {
-	//Copy the argument values, and bundle the message type into the object
+	// Copy the argument values, and bundle the message type into the object
 	Json::Value messageData = arguments;
 	messageData[MESSAGE_FIELD] = messageType;
 	
-	//Send the JSON data to the client (will happen on the networking thread's event loop)
+	// Send the JSON data to the client (will happen on the networking thread's event loop)
 	this->endpoint.send(conn, WebsocketServer::stringifyJson(messageData), websocketpp::frame::opcode::text);
 }
 
 void WebsocketServer::broadcastMessage(const string& messageType, const Json::Value& arguments)
 {
-	//Prevent concurrent access to the list of open connections from multiple threads
+	// Prevent concurrent access to the list of open connections from multiple threads
 	std::lock_guard<std::mutex> lock(this->connectionListMutex);
 	
 	for (auto conn : this->openConnections) {
@@ -77,14 +77,14 @@ void WebsocketServer::broadcastMessage(const string& messageType, const Json::Va
 void WebsocketServer::onOpen(ClientConnection conn)
 {
 	{
-		//Prevent concurrent access to the list of open connections from multiple threads
+		// Prevent concurrent access to the list of open connections from multiple threads
 		std::lock_guard<std::mutex> lock(this->connectionListMutex);
 		
-		//Add the connection handle to our list of open connections
+		// Add the connection handle to our list of open connections
 		this->openConnections.push_back(conn);
 	}
 	
-	//Invoke any registered handlers
+	// Invoke any registered handlers
 	for (auto handler : this->connectHandlers) {
 		handler(conn);
 	}
@@ -93,19 +93,19 @@ void WebsocketServer::onOpen(ClientConnection conn)
 void WebsocketServer::onClose(ClientConnection conn)
 {
 	{
-		//Prevent concurrent access to the list of open connections from multiple threads
+		// Prevent concurrent access to the list of open connections from multiple threads
 		std::lock_guard<std::mutex> lock(this->connectionListMutex);
 		
-		//Remove the connection handle from our list of open connections
+		// Remove the connection handle from our list of open connections
 		auto connVal = conn.lock();
 		auto newEnd = std::remove_if(this->openConnections.begin(), this->openConnections.end(), [&connVal](ClientConnection elem)
 		{
-			//If the pointer has expired, remove it from the vector
+			// If the pointer has expired, remove it from the vector
 			if (elem.expired() == true) {
 				return true;
 			}
 			
-			//If the pointer is still valid, compare it to the handle for the closed connection
+			// If the pointer is still valid, compare it to the handle for the closed connection
 			auto elemVal = elem.lock();
 			if (elemVal.get() == connVal.get()) {
 				return true;
@@ -114,11 +114,11 @@ void WebsocketServer::onClose(ClientConnection conn)
 			return false;
 		});
 		
-		//Truncate the connections vector to erase the removed elements
+		// Truncate the connections vector to erase the removed elements
 		this->openConnections.resize(std::distance(openConnections.begin(), newEnd));
 	}
 
-	//Invoke any registered handlers
+	// Invoke any registered handlers
 	for (auto handler : this->disconnectHandlers) {
 		handler(conn);
 	}
@@ -126,18 +126,18 @@ void WebsocketServer::onClose(ClientConnection conn)
 
 void WebsocketServer::onMessage(ClientConnection conn, WebsocketEndpoint::message_ptr msg)
 {
-	//Validate that the incoming message contains valid JSON
+	// Validate that the incoming message contains valid JSON
 	Json::Value messageObject = WebsocketServer::parseJson(msg->get_payload());
 	if (messageObject.isNull() == false)
 	{
-		//Validate that the JSON object contains the message type field
+		// Validate that the JSON object contains the message type field
 		if (messageObject.isMember(MESSAGE_FIELD))
 		{
-			//Extract the message type and remove it from the payload
+			// Extract the message type and remove it from the payload
 			std::string messageType = messageObject[MESSAGE_FIELD].asString();
 			messageObject.removeMember(MESSAGE_FIELD);
 			
-			//If any handlers are registered for the message type, invoke them
+			// If any handlers are registered for the message type, invoke them
 			auto& handlers = this->messageHandlers[messageType];
 			for (auto handler : handlers) {
 				handler(conn, messageObject);
--- server/server.cpp
+++ server/server.cpp
@@ -4,16 +4,16 @@
 #include <thread>
 #include <asio/io_service.hpp>
 
-//The port number the WebSocket server listens on
+// The port number the WebSocket server listens on
 #define PORT_NUMBER 8080
 
 int main(int argc, char* argv[])
 {
-	//Create the event loop for the main thread, and the WebSocket server
+	// Create the event loop for the main thread, and the WebSocket server
 	asio::io_service mainEventLoop;
 	WebsocketServer server;
 	
-	//Register our network callbacks, ensuring the logic is run on the main thread's event loop
+	// Register our network callbacks, ensuring the logic is run on the main thread's event loop
 	server.connect([&mainEventLoop, &server](ClientConnection conn)
 	{
 		mainEventLoop.post([conn, &server]()
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 			std::clog << "Connection opened." << std::endl;
 			std::clog << "There are now " << server.numConnections() << " open connections." << std::endl;
 			
-			//Send a hello message to the client
+			// Send a hello message to the client
 			server.sendMessage(conn, "hello", Json::Value());
 		});
 	});
@@ -43,38 +43,38 @@ int main(int argc, char* argv[])
 				std::clog << "\t" << key << ": " << args[key].asString() << std::endl;
 			}
 			
-			//Echo the message pack to the client
+			// Echo the message pack to the client
 			server.sendMessage(conn, "message", args);
 		});
 	});
 	
-	//Start the networking thread
+	// Start the networking thread
 	std::thread serverThread([&server]() {
 		server.run(PORT_NUMBER);
 	});
 	
-	//Start a keyboard input thread that reads from stdin
+	// Start a keyboard input thread that reads from stdin
 	std::thread inputThread([&server, &mainEventLoop]()
 	{
 		string input;
 		while (1)
 		{
-			//Read user input from stdin
+			// Read user input from stdin
 			std::getline(std::cin, input);
 			
-			//Broadcast the input to all connected clients (is sent on the network thread)
+			// Broadcast the input to all connected clients (is sent on the network thread)
 			Json::Value payload;
 			payload["input"] = input;
 			server.broadcastMessage("userInput", payload);
 			
-			//Debug output on the main thread
+			// Debug output on the main thread
 			mainEventLoop.post([]() {
 				std::clog << "User input debug output on the main thread" << std::endl;
 			});
 		}
 	});
 	
-	//Start the event loop for the main thread
+	// Start the event loop for the main thread
 	asio::io_service::work work(mainEventLoop);
 	mainEventLoop.run();
 	
